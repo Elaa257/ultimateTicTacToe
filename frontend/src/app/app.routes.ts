ae57f0c498aad75a0192c938ac78ab6aa55c0e4b
Gabriela Krause <gabriela.krause@mni.thm.de>
2024-08-16 20:43:22 +0200

    rebasing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -10,6 +10,7 @@ export const routes: Routes = [
   { path: 'auth', component: AuthComponent },
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
   { path: 'home', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent}
   { path: 'notfound', component: PageNotFoundComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
