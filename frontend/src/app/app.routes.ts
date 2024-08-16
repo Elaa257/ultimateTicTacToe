cccf366c91aa272e57bf05ebfcbed89747806b40
Gabriela Krause <gabriela.krause@mni.thm.de>
2024-08-16 10:49:39 +0200

    Added nav-bar functionality and responsiveness (still has a bug)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,17 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent} from './auth/auth.component';
 import { ProfileComponent } from './profile/profile.component';
 import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
   { path: 'auth', component: AuthComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'home', component: HomeComponent },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
