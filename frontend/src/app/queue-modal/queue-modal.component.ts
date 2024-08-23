64dcdab4d39d5a1795af8463120452c2b8fd704c
Jan Weis <jan-luca.weis@mni.thm.de>
2024-08-23 11:45:49 +0200

    - added a timer and some small style stuff

--- frontend/src/app/queue-modal/queue-modal.component.ts
+++ frontend/src/app/queue-modal/queue-modal.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
 import { WebSocketService } from './web-socket.service';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-queue-modal',
   templateUrl: './queue-modal.component.html',
   standalone: true,
   styleUrls: ['./queue-modal.component.css'],
+  imports: [
+    MatButton,
+    MatDialogClose,
+    MatDialogActions,
+    MatDialogContent,
+    MatDialogTitle,
+  ],
 })
 export class QueueModalComponent {
   canStartGame = false; // Initially false
+  timeInQueue: number = 0;
+  timer:any;
 
   constructor(
     public dialogRef: MatDialogRef<QueueModalComponent>,
@@ -29,7 +45,34 @@ export class QueueModalComponent {
 
   startGame(): void {
     console.log('Start Game button clicked');
+    this.stopTimer()
     this.webSocketService.emit('start-game');
     this.dialogRef.close();
+
+  }
+  ngOnInit(){
+    this.startTimer()
+  }
+  ngOnDestroy(){
+    this.stopTimer();
+  }
+  startTimer(){
+    this.timer = setInterval(()=>{
+      this.timeInQueue++
+    },1000)
+  }
+  stopTimer(){
+    if(this.timer){
+      clearInterval(this.timer);
+    }
+  }
+  formatTime(sec:number){
+    const minutes = Math.floor((sec/60));
+    const seconds = sec%60;
+    return `${this.pad(minutes)}:${this.pad(seconds)}`
+  }
+
+  pad(value:number){
+    return value <10 ? `0${value}` : `${value}`;
   }
 }
