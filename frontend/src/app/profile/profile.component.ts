456da8eb47aa2c2e188e2cd265b0a390525f5a53
Jan Weis <jan-luca.weis@mni.thm.de>
2024-09-07 13:55:09 +0200

    - added a timer and some small style stuff

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
+import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardImage } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatCardActions,
+    MatButton,
+    MatCardImage,
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
